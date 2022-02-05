102f2bf9d8dd2b846c4f43aa4f1ba53623524d63
Alina <80460626+alishney@users.noreply.github.com>
2022-02-05 13:56:29 +0300

    added cards

--- components/footer.php
+++ components/footer.php
@@ -1,10 +1,14 @@
 </main>
 <footer>
     <div class="footer__left">
-        <a href="https://www.pinterest.ru/search/pins/?q=cosmetic" title="pinterest" target="_blank"><i class="fab fa-pinterest"></i></a>
-        <a href="https://www.facebook.com/OfficialMAKEUP/" title="facebook" target="_blank"><i class="fab fa-facebook"></i></a>
-        <a href="https://twitter.com/hashtag/makeup" title="twitter target="_blank" "><i class="fab fa-twitter"></i></a>
-        <a href="https://www.instagram.com/makeup.kitchen/?hl=en" title="instagram" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.pinterest.ru/search/pins/?q=cosmetic" title="pinterest" target="_blank"><i
+               class="fab fa-pinterest"></i></a>
+        <a href="https://www.facebook.com/OfficialMAKEUP/" title="facebook" target="_blank"><i
+               class="fab fa-facebook"></i></a>
+        <a href="https://twitter.com/hashtag/makeup" title="twitter" target=" _blank"><i class=" fab
+           fa-twitter"></i></a>
+        <a href="https://www.instagram.com/makeup.kitchen/?hl=en" title="instagram" target="_blank"><i
+               class="fab fa-instagram"></i></a>
     </div>
     <div class="footer__right">
         <form method="get" action="./?page=home">
@@ -20,6 +24,7 @@
         </form>
     </div>
 </footer>
-<p style="text-align: center; margin-bottom: 4rem;">Алина шнейдер 2022 &copy;</p>
+<p style="text-align: center; margin-bottom: 4rem;">Алина Шнейдер 2022 &copy;</p>
 </body>
+
 </html>
